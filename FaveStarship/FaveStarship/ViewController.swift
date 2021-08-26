b4e39e0c31d869e49170c8f72e6522569a18ae91
arvinq <arvin.quiliza@gmail.com>
2021-08-27 06:17:51 +1000

    Project creation

---
+++ FaveStarship/FaveStarship/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaveStarship
+//
+//  Created by Arvin Quiliza on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
